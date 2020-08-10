5735f21848336f7723c1ad9688fbbab67bbf66ba
beton726 <909043377fas>
2020-08-11 01:57:50 +0300

    Write REST

--- src/main/java/dev/yusov/Solution.java
+++
@@ -1,4 +0,0 @@
-package dev.yusov;
-
-public class Solution {
-}
---
+++ src/main/java/dev/yusov/model/Client.java
@@ -0,0 +1,15 @@
+package dev.yusov.model;
+
+public class Client {
+
+    private Integer id;
+    private String name;
+    private String number;
+
+    public Integer getId() {
+        return id;
+    }
+
+    
+
+}
